515b3bde1e36542af91712818ebffd60802f3dc0
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2021-08-10 03:23:43 +0200

    Update rtw_security.h

--- include/rtw_security.h
+++ include/rtw_security.h
@@ -244,11 +244,11 @@ struct security_priv {
 #endif /* DBG_SW_SEC_CNT */
 };
 
-/*struct sha256_state {
+struct sha256_state {
 	u64 length;
 	u32 state[8], curlen;
 	u8 buf[64];
-};*/
+};
 
 #define GET_ENCRY_ALGO(psecuritypriv, psta, encry_algo, bmcst)\
 	do {\
